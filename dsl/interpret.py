2a83b0759a80bad42bbca57e4c80147cc1fe756e
DanChirtoaca <chirtoaca.dan@gmail.com>
2019-05-13 18:01:49 +0200

    Fix grammar to be properly parsed by textX

--- dsl/interpret.py
+++ dsl/interpret.py
@@ -1,3 +1,4 @@
+"""
 from textx import metamodel_from_file
 
 meta_model = metamodel_from_file('grammar.tx')
@@ -5,3 +6,9 @@ meta_model = metamodel_from_file('grammar.tx')
 nft_model = meta_model.model_from_file('model.nft')
 
 print (nft_model)
+
+"""
+from mako.template import Template
+
+mytemplate = Template(filename='model.nft')
+print(mytemplate.render())
