f5b723d2d221e1a9c1524e21fc8ad1b7cfbc610e
DanChirtoaca <chirtoaca.dan@gmail.com>
2019-05-21 18:22:12 +0200

    Add support for sized integers as in solidity.

--- dsl/interpret.py
+++ dsl/interpret.py
@@ -1,4 +1,4 @@
-"""
+
 from textx import metamodel_from_file
 
 meta_model = metamodel_from_file('grammar.tx')
@@ -6,6 +6,7 @@ meta_model = metamodel_from_file('grammar.tx')
 nft_model = meta_model.model_from_file('model.nft')
 
 print (nft_model)
+
 
 """
 from mako.template import Template
@@ -17,7 +18,7 @@ data['imports'] = ["./File1.sol", "./File2.sol", "./File3.sol"]
 data['extensions'] = ["File1", "File2", "File3"]
 print(mytemplate.render(data=data))
 
-
+"""
 # functions accepting modifiers:
 '''
 1. ERC721Base
